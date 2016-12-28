27314f645daaa10479757754871067fd206e444b
Jake Whatley <jacob.whatley91@gmail.com>
2016-12-28 04:30:33 +0100

    ether: enable ANT+
    Change-Id: Ic55dd3daa4fb94249e507a2e07a5030cac5bb1f2

--- device.mk
+++ device.mk
@@ -57,14 +57,14 @@ PRODUCT_COPY_FILES += \
 
 
 # ANT+
-#PRODUCT_PACKAGES += \
-#    AntHalService \
-#    com.dsi.ant.antradio_library \
-#    libantradio
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
 
 # Permissions
-#PRODUCT_COPY_FILES += \
-#    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
