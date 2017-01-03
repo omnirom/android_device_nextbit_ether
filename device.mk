15eb9fdd16c7bf155e13277e7b91b400844576f3
Jake Whatley <jacob.whatley91@gmail.com>
2017-01-03 05:11:22 +0100

    ether: use AOSP GPS HAL
    Change-Id: Ifd618cd98ce786b61cd8a38fc03354281dd2c469

--- device.mk
+++ device.mk
@@ -303,12 +303,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
 
+PRODUCT_PACKAGES += \
+    gps.msm8992
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=0 \
