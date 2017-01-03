547c2f7100ad1561fade2bc3c4f09782de5cdb7a
Jake Whatley <jacob.whatley91@gmail.com>
2017-01-03 05:43:18 +0100

    ether: remove CAF telephony packages, use AOSP RIL
    Change-Id: Idb9e5d66d3c8e377311d71088b2cef0df43a9c46

--- device.mk
+++ device.mk
@@ -439,6 +439,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.iwlan.enable=true
 
 # Telephony extension
-PRODUCT_PACKAGES += telephony-ext
-PRODUCT_PACKAGES += ims-ext-common
-PRODUCT_BOOT_JARS += telephony-ext
+#PRODUCT_PACKAGES += telephony-ext
+#PRODUCT_PACKAGES += ims-ext-common
+#PRODUCT_BOOT_JARS += telephony-ext
