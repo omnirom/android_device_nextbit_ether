96fcb26be4937f73b83a624b55d33ab59d68c8dc
Jake Whatley <jacob.whatley91@gmail.com>
2017-02-14 03:00:30 -0500

    RobinParts: allow toggling Fingerprint Wake and Unlock
    Change-Id: Id6d33b7c1c39e109fb864f2c336fad6ed30f575f

---
+++ RobinParts/src/org/omnirom/device/DeviceSettings.java
@@ -0,0 +1,78 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.content.res.Resources;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.TwoStatePreference;
+import android.provider.Settings;
+import android.view.MenuItem;
+import android.util.Log;
+
+public class DeviceSettings extends PreferenceActivity implements
+        Preference.OnPreferenceChangeListener {
+
+    public static final String KEY_FINGERPRINT_WAKE_UNLOCK = "fingerprint";
+
+    private TwoStatePreference mFingerprintWakeUnlcok;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addPreferencesFromResource(R.xml.main);
+
+        mFingerprintWakeUnlcok = (TwoStatePreference) findPreference(KEY_FINGERPRINT_WAKE_UNLOCK);
+        mFingerprintWakeUnlcok.setChecked(Settings.System.getInt(getContentResolver(),
+                    Settings.System.FINGERPRINT_WAKE_UNLOCK, 0) != 0);
+     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mFingerprintWakeUnlock) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.FINGERPRINT_WAKE_UNLOCK, mFingerprintWakeUnlcok.isChecked() ? 1 : 0);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return true;
+    }
+}
