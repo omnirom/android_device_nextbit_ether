daa543bc037d47ff17c441a69ed1c6ce41ec8c82
Michael Bestas <mkbestas@lineageos.org>
2017-07-24 19:49:09 +0300

    ether: Configure TWRP
    Change-Id: Id2e75d03c8b967450947222d174dd8a1c99762e1

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,7 @@ fi
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
+extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC"
 
 "$MY_DIR"/setup-makefiles.sh
 
--- omni_ether.mk
+++ omni_ether.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/nextbit/ether/device-vendor.mk)
 
 # Copy TWRP fstab - TWRP requires fstab v1
 PRODUCT_COPY_FILES += \
-    device/nextbit/ether/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
+    device/nextbit/ether/twrp/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
