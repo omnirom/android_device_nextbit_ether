d5903827f01d69adf39ffbc1de8cbef2bd0f0feb
Jacob Whatley <jake@whatley.us>
2018-04-20 21:59:06 +0200

    fingerprint: build the 2.1 hal
    * also copy OEM fingerprint hal to vendor and rename accordingly for the wrapper
    
    Change-Id: Ie664e7db41e8ae47409f1196f0e92be303b253f1

--- device.mk
+++ device.mk
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service \
+    android.hardware.biometrics.fingerprint@2.1-service \
     fingerprint.msm8992
 
 # For android_filesystem_config.h
